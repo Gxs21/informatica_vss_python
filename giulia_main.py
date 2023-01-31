25cc85cb925e2ba8add21bc0d780ae9ffba2e1fb
gxs21 <giulio.sodero4@gmail.com>
2023-01-31 21:18:59 +0100

    add new line

--- giulia_main.py
+++ giulia_main.py
@@ -5,15 +5,15 @@ def main():
     saldo=int(input("iserisci il saldo attuale: "))
     conto = Contobancario (nr_conto, nominativo, saldo, 0)
     while True:
-        n=str(input("per prelevare inserisci p , per versare v"))
+        n=str(input("per prelevare inserisci:  p , per versare v: "))
         if n=="p":
             Prelievo=int(input("inserisci la somma he vuoi prelevare"))
-        if Prelievo<conto.getSaldo():
-            conto.prelievo (saldo,Prelievo)
+        if Prelievo<conto.saldo:
+            conto.prelievo (Prelievo,)
         if n=="v":
             Versamento=int(input("inserisci la somma che vuoi versare"))
             conto.versamento (Versamento)
-        conto.stampa_conto
+        conto.stampa_conto()
 main ()
 
 
