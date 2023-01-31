6092c0d72037987261c8992929e3c5e4f6649a78
gxs21 <giulio.sodero4@gmail.com>
2023-01-31 21:11:59 +0100

    add new line

--- giulia_main.py
+++ giulia_main.py
@@ -1,20 +1,19 @@
-from classecontobancario input Contobancario
-def main()
-nr_conto=int(input ("inserire il numero del conto"))
-nominativo=input ("inserire il suo nominativo")
-saldo=int(input("iserisci il saldo attuale"))
-conto=Contobancario (nr_conto,nominativo,saldo)
-prosegui=p
-while prosegui==1:
-    n=str(input("per prelevare inserisci p , per versare v"))
-    if n==p:
-        Prelievo=int(input("inserisci la somma he vuoi prelevare"))
+from giulia_classe import Contobancario
+def main():
+    nr_conto=int(input ("inserire il numero del conto: "))
+    nominativo=input ("inserire il suo nominativo: ")
+    saldo=int(input("iserisci il saldo attuale: "))
+    conto = Contobancario (nr_conto, nominativo, saldo, 0)
+    while True:
+        n=str(input("per prelevare inserisci p , per versare v"))
+        if n=="p":
+            Prelievo=int(input("inserisci la somma he vuoi prelevare"))
         if Prelievo<conto.getSaldo():
             conto.prelievo (saldo,Prelievo)
-    if n==v:
-        Versamento=int(input("inserisci la somma che vuoi versare"))
-        conto.versamento (Versamento)
-
+        if n=="v":
+            Versamento=int(input("inserisci la somma che vuoi versare"))
+            conto.versamento (Versamento)
+        conto.stampa_conto
 main ()
 
 
