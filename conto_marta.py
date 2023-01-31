a920f64263472d28a8cd0a1a2cc234cca816abdd
gxs21 <giulio.sodero4@gmail.com>
2023-01-31 21:37:18 +0100

    add new line

--- conto_marta.py
+++ conto_marta.py
@@ -28,4 +28,4 @@ class ContoBancario:
             print("non puoi farlo, mi dispiace")
 
     def stampa (self):
-        print ('il numero di conto è: ', self.nr_conto,' il nominativo del conto è:  ',str(self.nominativo), "il saldo ammonta a: ", self.saldo, " il numero di operazioni effettuate ammonta a: ", self.numero_operazioni)
+        print ('il numero di conto è: ', self.nr_conto,' il nominativo del conto è:  ',self.nominativo, "il saldo ammonta a: ", self.saldo, " il numero di operazioni effettuate ammonta a: ", self.numero_operazioni)
