a5420bdcac3ae44d7007e4144fd6587bf004a5aa
gxs21 <giulio.sodero4@gmail.com>
2023-03-16 22:47:35 +0100

    add new line

--- ospedali/main_reb.py
+++ ospedali/main_reb.py
@@ -2,7 +2,7 @@
 from ospedale_reb import Ospedale
 from paziente_reb import Paziente
 def main():
-    p1=Paziente(1,3, 4, 5, 6)  
+    p1=Paziente("uno",3, 4, 5, 6)  
     p2=Paziente(2,"Verdi","Giuseppe","Gastroenterite","Verde")
     p3=Paziente(3,"Bianchi","Francesco","Frattura omero","Giallo")
     p4=Paziente(4,"Ferrari","Luigi","Infarto","Rosso")
@@ -11,7 +11,10 @@ def main():
     o1=Ospedale("Vitofazzi","Lecce")
     print(p1.numeroletto)
     o1.aggiungiPaziente(p2)
-    """o1.dimettiPaziente(2)"""   
+    """print(p2.codicericovero)
+    p2.toString()
+    o1.dimettiPaziente(2)
+     """
     o1.visualizzaPazienti("verde")
     
 main()
