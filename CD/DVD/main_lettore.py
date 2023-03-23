0862796e615367e107a4274d06c76733fc119002
gxs21 <giulio.sodero4@gmail.com>
2023-03-24 00:02:42 +0100

    add new line

--- CD/DVD/main_lettore.py
+++ CD/DVD/main_lettore.py
@@ -4,5 +4,5 @@ def main():
     supporto1= supporto(34, "davide", "gemitaiz", 13, "CD", 95, 2018)
     supporto2= supporto(31, "scatola nera", "gemitaiz", 14, "cd", 89, 2020)
     supporto1.toString()
-    print (supporto1.confronta(supporto2))
+    """print (supporto1.confronta(supporto2))"""
 main()
